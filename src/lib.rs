1daa0f2304e5d4487ecfd1fcd443af3591020109
Azz <azz@valera.co>
2023-03-18 19:32:39 +0000

    various bits

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,47 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+
+enum Message {
+    /// A message from the client when it calls a method on the server.
+    RPCRequest {
+        /// A unique counter for each RPC call.
+        /// This is used to match responses to requests. It restricts the number of
+        /// concurrent operations to 256. Active operations cannot reuse the same ID,
+        /// therefore IDs of completed requests can be reused.
+        id: u8,
+        /// The internal message serialized with rkyv. This will include the 
+        /// method name and arguments. The format of this message will slightly 
+        /// differ depending on the number of methods, and types of arguments.
+        /// The macros handle generating the code for this.
+        internal: Vec<u8>
+    }, 
+    /// A message from the server with the output of a client's method call.
+    RPCResponse {
+        /// A unique counter for each RPC call.
+        /// This is used to match responses to requests. It restricts the number of
+        /// concurrent operations to 256. Active operations cannot reuse the same ID,
+        /// therefore IDs of completed requests can be reused.
+        id: u8,
+        /// The function's output serialized with rkyv. The format of this 
+        /// message will differ with each application.
+        /// The macros handle generating the code for this.
+        output: Vec<u8>
+    }, 
+    /// A message from the server with a new event.
+    NewEvent {
+        /// The event serialized with rkyv. The format of this will differ 
+        /// between applications. The macros handle generating the code for this.
+        event: Vec<u8>
+    }, 
+    /// The server updates the connection state.
+    StateChange(StateChange),
+}
+
+enum StateChange {
+    /// A key and value have been inserted into the map.
+    /// The value will differ depending on the application.
+    Insert(String, Vec<u8>),
+    /// A key has been removed from the map. 
+    Remove(String),
+}
